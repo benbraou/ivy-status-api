e8443258b0faccafa41f6a065bbb69f73b33f6f1
Oussama Ben Brahim <benbraou@gmail.com>
2018-03-18 18:04:32 +0100

    add constants

---
+++ business/constants.go
@@ -0,0 +1,11 @@
+// Copyright (C) Oussama Ben Brahim - All Rights Reserved
+// Use of this source code is governed by a MIT License that can be found in
+// https://github.com/benbraou/ivy-status-api/blob/main/LICENSE
+
+package business
+
+const (
+	CROSS = "\u274C"
+	CHECK = "\u2705"
+	NA    = "n/a "
+)
