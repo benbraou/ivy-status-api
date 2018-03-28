3edf77b9410e154c28952aaee7c99733dfcd480c
Oussama Ben Brahim <benbraou@gmail.com>
2018-03-28 23:59:45 +0200

    handle constants in different package

---
+++ constants/const.go
@@ -0,0 +1,17 @@
+// Copyright (C) Oussama Ben Brahim - All Rights Reserved
+// Use of this source code is governed by a MIT License that can be found in
+// https://github.com/benbraou/ivy-status-api/blob/main/LICENSE
+
+package constants
+
+const (
+	// Cross = ✅
+	Cross = "\u274C"
+	// Check = ❌
+	Check = "\u2705"
+	// Na    = "n/a"
+	Na = "n/a"
+	// RootFeatureGroupName is the name of the root feature group that will contain all feature groups
+	// defined in Angular `STATUS.md`
+	RootFeatureGroupName = "root"
+)
