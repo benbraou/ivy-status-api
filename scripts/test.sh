298e72ea69a18b8c4439a1eb7006afd1fe94c601
Oussama Ben Brahim <benbraou@gmail.com>
2018-03-30 21:24:05 +0200

    colorize test results

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,4 @@
-go test -v ../...
+go test -v ../... \
+| sed ''/RUN/s//$(printf "\033[36mRUN\033[0m")/'' \
+| sed ''/PASS/s//$(printf "\033[32mPASS\033[0m")/'' \
+| sed ''/FAIL/s//$(printf "\033[1;31mFAIL\033[0m")/''
